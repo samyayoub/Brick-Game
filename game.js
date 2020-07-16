3c00a26a3ed3aba0e6014398cfb61b2b12f6bb61
Samy Ayoub <samyayoub@gmail.com>
2020-07-16 00:02:11 -0500

    Adding a paddle and the moving functionality

--- game.js
+++ game.js
@@ -5,11 +5,25 @@ var x = canvas.width / 2;
 var y = canvas.height - 30;
 var dx = 2;
 var dy = -2;
+var ballRadius = 10;
+var paddleHeight = 10;
+var paddleWidth = 75;
+var paddleX = (canvas.width - paddleWidth) / 2;
+var rightPressed = false;
+var leftPressed = false;
 
 function drawBall() {
     ctx.beginPath();
-    ctx.arc(x, y, 10, 0, Math.PI * 2);
+    ctx.arc(x, y, ballRadius, 0, Math.PI * 2);
     ctx.fillStyle = "orange";
+    ctx.fill();
+    ctx.closePath();
+}
+
+function drawPaddle() {
+    ctx.beginPath();
+    ctx.rect(paddleX, canvas.height - paddleHeight, paddleWidth, paddleHeight);
+    ctx.fillStyle = "black";
     ctx.fill();
     ctx.closePath();
 }
@@ -17,8 +31,50 @@ function drawBall() {
 function draw() {
     ctx.clearRect(0, 0, canvas.width, canvas.height);
     drawBall();
+    if ((x + ballRadius) > canvas.width || (x - ballRadius) < 0) {
+        dx = -dx
+    }
+
+    if ((y + ballRadius) > canvas.height || (y - ballRadius) < 0) {
+        dy = -dy;
+    }
+
     x += dx;
     y += dy;
+    drawPaddle();
+    if (rightPressed) {
+        paddleX += 5;
+        if (paddleX + paddleWidth > canvas.width) {
+            paddleX = canvas.width - paddleWidth;
+        }
+    }
+    else if (leftPressed) {
+        paddleX -= 5;
+        if (paddleX < 0) {
+            paddleX = 0;
+        }
+    }
 }
+
+function keyDownHandler(e) {
+    if (e.key == "Right" || e.key == "ArrowRight") {
+        rightPressed = true;
+    }
+    else if (e.key == "Left" || e.key == "ArrowLeft") {
+        leftPressed = true;
+    }
+}
+
+function keyUpHandler(e) {
+    if (e.key == "Right" || e.key == "ArrowRight") {
+        rightPressed = false;
+    }
+    else if (e.key == "Left" || e.key == "ArrowLeft") {
+        leftPressed = false;
+    }
+}
+
+document.addEventListener("keydown", keyDownHandler, false);
+document.addEventListener("keyup", keyUpHandler, false);
 
 setInterval(draw, 10);
