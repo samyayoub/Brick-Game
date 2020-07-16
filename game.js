e29d76ee9bd3eee0a685990a70f98cff286267a0
Samy Ayoub <samyayoub@gmail.com>
2020-07-16 12:57:12 -0500

    Drawing the bricks

--- game.js
+++ game.js
@@ -12,12 +12,45 @@ var paddleX = (canvas.width - paddleWidth) / 2;
 var rightPressed = false;
 var leftPressed = false;
 
+// Variables for bricks
+var brickRowCount = 3;
+var brickColumnCount = 7;
+var brickWidth = 75;
+var brickHeight = 20;
+var brickPadding = 10;
+var brickOffsetTop = 30;
+var brickOffsetLeft = 30;
+
+var bricks = [];
+for (var c = 0; c < brickColumnCount; c++) {
+    bricks[c] = [];
+    for (var r = 0; r < brickRowCount; r++) {
+        bricks[c][r] = { x: 0, y: 0 };
+    }
+}
+
 function drawBall() {
     ctx.beginPath();
     ctx.arc(x, y, ballRadius, 0, Math.PI * 2);
     ctx.fillStyle = "orange";
     ctx.fill();
     ctx.closePath();
+}
+
+function drawBricks() {
+    for (var c = 0; c < brickColumnCount; c++) {
+        for (var r = 0; r < brickRowCount; r++) {
+            var brickX = (c * (brickWidth + brickPadding)) + brickOffsetLeft;
+            var brickY = (r * (brickHeight + brickPadding)) + brickOffsetTop;
+            bricks[c][r].x = brickX;
+            bricks[c][r].y = brickY;
+            ctx.beginPath();
+            ctx.rect(brickX, brickY, brickWidth, brickHeight);
+            ctx.fillStyle = "green";
+            ctx.fill();
+            ctx.closePath();
+        }
+    }
 }
 
 function drawPaddle() {
@@ -30,18 +63,29 @@ function drawPaddle() {
 
 function draw() {
     ctx.clearRect(0, 0, canvas.width, canvas.height);
+
     drawBall();
+    drawBricks();
+    drawPaddle();
+
     if ((x + ballRadius) > canvas.width || (x - ballRadius) < 0) {
         dx = -dx
     }
 
-    if ((y + ballRadius) > canvas.height || (y - ballRadius) < 0) {
+    if ((y - ballRadius) < 0) {
         dy = -dy;
+    } else if ((y + ballRadius) > canvas.height) {
+        if (x > paddleX && x < (paddleX + paddleWidth)) {
+            dy = -dy;
+        }
+        else {
+            document.location.reload();
+            clearInterval(interval); //Needed for Chrome to end game
+        }
     }
 
     x += dx;
     y += dy;
-    drawPaddle();
     if (rightPressed) {
         paddleX += 5;
         if (paddleX + paddleWidth > canvas.width) {
@@ -54,6 +98,8 @@ function draw() {
             paddleX = 0;
         }
     }
+
+
 }
 
 function keyDownHandler(e) {
@@ -77,4 +123,4 @@ function keyUpHandler(e) {
 document.addEventListener("keydown", keyDownHandler, false);
 document.addEventListener("keyup", keyUpHandler, false);
 
-setInterval(draw, 10);
+var interval = setInterval(draw, 10);
