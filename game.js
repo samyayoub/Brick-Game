90d59bbf42d32f2a9c60c29b9ff54dbec0dca20b
Samy Ayoub <samyayoub@gmail.com>
2020-07-17 22:34:27 -0500

    Track Score and win; Added Comments through the code

--- game.js
+++ game.js
@@ -21,6 +21,9 @@ var brickPadding = 10;
 var brickOffsetTop = 30;
 var brickOffsetLeft = 30;
 
+var score = 0;
+
+// Creating the bricks array
 var bricks = [];
 for (var c = 0; c < brickColumnCount; c++) {
     bricks[c] = [];
@@ -29,6 +32,7 @@ for (var c = 0; c < brickColumnCount; c++) {
     }
 }
 
+// Function to draww the ball
 function drawBall() {
     ctx.beginPath();
     ctx.arc(x, y, ballRadius, 0, Math.PI * 2);
@@ -37,10 +41,12 @@ function drawBall() {
     ctx.closePath();
 }
 
+// Function to draw all the bricks
 function drawBricks() {
     for (var c = 0; c < brickColumnCount; c++) {
         for (var r = 0; r < brickRowCount; r++) {
             if (bricks[c][r].status === 1) {
+                // Create each brick accordding to the given # of columns, rows and their sizes
                 var brickX = (c * (brickWidth + brickPadding)) + brickOffsetLeft;
                 var brickY = (r * (brickHeight + brickPadding)) + brickOffsetTop;
                 bricks[c][r].x = brickX;
@@ -55,6 +61,7 @@ function drawBricks() {
     }
 }
 
+// Function to draw the paddle controlled by user
 function drawPaddle() {
     ctx.beginPath();
     ctx.rect(paddleX, canvas.height - paddleHeight, paddleWidth, paddleHeight);
@@ -63,6 +70,7 @@ function drawPaddle() {
     ctx.closePath();
 }
 
+// Function to check whenever button is pressed
 function keyDownHandler(e) {
     if (e.key == "Right" || e.key == "ArrowRight") {
         rightPressed = true;
@@ -72,6 +80,7 @@ function keyDownHandler(e) {
     }
 }
 
+// Function to check whenever button is released
 function keyUpHandler(e) {
     if (e.key == "Right" || e.key == "ArrowRight") {
         rightPressed = false;
@@ -81,39 +90,63 @@ function keyUpHandler(e) {
     }
 }
 
+// Function to check for collision
 function collisionDetector() {
     for (var c = 0; c < brickColumnCount; c++) {
         for (var r = 0; r < brickRowCount; r++) {
             var brickObject = bricks[c][r];
+
+            // Checking if the block is visible or not; if visible then change direction, change it invisible and increase score
             if (brickObject.status === 1) {
                 if (x > brickObject.x && x < brickObject.x + brickWidth && y > brickObject.y && y < brickObject.y + brickHeight) {
                     dy = -dy;
                     brickObject.status = 0;
+                    score++;
+
+                    // Alert winning if all bricks are cleared
+                    if (score == brickRowCount * brickColumnCount) {
+                        alert("You did it. Congrats!");
+                        document.location.reload();
+                        clearInterval(interval);
+                    }
                 }
             }
         }
     }
 }
 
+// Function to draw the score
+function drawScore() {
+    ctx.font = "16px Arial";
+    ctx.fillStyle = "#0095DD";
+    ctx.fillText("Score: " + score, 8, 20);
+}
+
+// Function to draw everything
 function draw() {
     ctx.clearRect(0, 0, canvas.width, canvas.height);
 
     drawBall();
     drawBricks();
     drawPaddle();
+    drawScore();
 
     collisionDetector();
 
+    // Check if ball hits the left or right walls. If ball hits, then it will switch direction.
     if ((x + ballRadius) > canvas.width || (x - ballRadius) < 0) {
         dx = -dx
     }
 
+    // Check if ball hits top wall. If ball hits, then it will switch direction.
     if ((y - ballRadius) < 0) {
         dy = -dy;
     } else if ((y + ballRadius) > canvas.height) {
+        // Check if ball hits paddle.
         if (x > paddleX && x < (paddleX + paddleWidth)) {
             dy = -dy;
         }
+        // End game if ball misses the paddle (hits the bottom wall)
         else {
             document.location.reload();
             clearInterval(interval); //Needed for Chrome to end game
@@ -122,6 +155,8 @@ function draw() {
 
     x += dx;
     y += dy;
+
+    // check for buttons pressed and move paddle accordingly
     if (rightPressed) {
         paddleX += 5;
         if (paddleX + paddleWidth > canvas.width) {
@@ -136,6 +171,7 @@ function draw() {
     }
 }
 
+// Event listeners to the buttons
 document.addEventListener("keydown", keyDownHandler, false);
 document.addEventListener("keyup", keyUpHandler, false);
 
